b421c6f45457cfecbfe4be7d96ed9a472ec440a6
BrendaALandaC <balc19@hotmail.com>
2026-05-06 12:19:31 -0600

    feat: add auth DTOs and repositories

---
+++ src/main/java/com/brenda/courseflow/auth/dto/LoginRequest.java
@@ -0,0 +1,16 @@
+package com.brenda.courseflow.auth.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
---
+++ src/main/java/com/brenda/courseflow/auth/dto/LoginResponse.java
@@ -0,0 +1,11 @@
+package com.brenda.courseflow.auth.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class LoginResponse {
+
+    private String token;
+}
