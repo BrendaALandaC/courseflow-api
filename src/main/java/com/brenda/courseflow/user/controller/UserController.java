c5107b3e116ab277aa89f6a87461735e072ffc52
BrendaALandaC <balc19@hotmail.com>
2026-06-07 22:41:08 -0600

    feat: complete user CRUD

--- src/main/java/com/brenda/courseflow/user/controller/UserController.java
+++ src/main/java/com/brenda/courseflow/user/controller/UserController.java
@@ -2,9 +2,12 @@ package com.brenda.courseflow.user.controller;
 
 import com.brenda.courseflow.user.dto.UserCreateRequest;
 import com.brenda.courseflow.user.dto.UserResponse;
+import com.brenda.courseflow.user.dto.UserUpdateRequest;
 import com.brenda.courseflow.user.service.UserService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -47,4 +50,27 @@ public class UserController {
 
         return userService.create(request);
     }
+
+
+    @Operation(summary = "Update user", description = "Update user",
+            responses={ @ApiResponse(responseCode="200", description="OK")})
+    @PutMapping("/{id}")
+    public UserResponse update(
+            @PathVariable Long id,
+            @RequestBody @Valid UserUpdateRequest request
+    ) {
+        return userService.update(id, request);
+    }
+
+
+    @Operation(summary = "Delete user", description = "Delete user")
+    @ApiResponses({
+            @ApiResponse(responseCode = "204", description = "OK"),
+            @ApiResponse(responseCode = "400", description = "Not found", content = @Content)
+    })
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable Long id) {
+        userService.delete(id);
+    }
+
 }
--- src/main/java/com/brenda/courseflow/user/service/UserService.java
+++ src/main/java/com/brenda/courseflow/user/service/UserService.java
@@ -6,6 +6,7 @@ import com.brenda.courseflow.shared.exception.BadRequestException;
 import com.brenda.courseflow.shared.exception.ResourceNotFoundException;
 import com.brenda.courseflow.user.dto.UserCreateRequest;
 import com.brenda.courseflow.user.dto.UserResponse;
+import com.brenda.courseflow.user.dto.UserUpdateRequest;
 import com.brenda.courseflow.user.entity.User;
 import com.brenda.courseflow.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -66,6 +67,36 @@ public class UserService {
         userRepository.save(user);
 
         return mapToResponse(user);
+    }
+
+    public UserResponse update(Long id, UserUpdateRequest request) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        if (userRepository.existsByUsernameAndIdNot(request.getUsername(), id)) {
+            throw new BadRequestException("Username already exists");
+        }
+
+        if (userRepository.existsByEmailAndIdNot(request.getEmail(), id)) {
+            throw new BadRequestException("Email already exists");
+        }
+
+        Role role = roleRepository.findByName(request.getRole())
+                .orElseThrow(() -> new ResourceNotFoundException("Role not found"));
+
+        user.setFullName(request.getFullName());
+        user.setEmail(request.getEmail());
+        user.setUsername(request.getUsername());
+        user.setRole(role);
+
+        return mapToResponse(userRepository.save(user));
+    }
+
+    public void delete(Long id) {
+        User user = userRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("User not found"));
+
+        userRepository.delete(user);
     }
 
     private UserResponse mapToResponse(User user) {
